b8f2d0d72f233bbf8116c0922dbc5aad3760cd1c
kamoliddin0105 <kamoliddinshukurov967@gmail.com>
2024-12-27 09:41:33 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+avtolar = ['bmw','mers','lacetti','volvo','kia']
+for avto in avtolar:
+    if avto == 'bmw':
+        print(avto.upper())
+    elif avto == 'mers':
+        print('Mercedes-Benz')
+    else:
+        print(avto)
