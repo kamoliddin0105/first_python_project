16a65d548f3dea690669c0e0ca4bf203bf373691
kamoliddin0105 <kamoliddinshukurov967@gmail.com>
2025-01-04 09:36:35 +0500

    Functions with printing

---
+++ functions/functions.py
@@ -0,0 +1,28 @@
+# print("Salom beruvchi funksiya")
+# def salom_ber(ism):
+#     print(f"Assalomu alaykum {ism.title()}")
+#
+# salom_ber("Kamoliddin")
+
+def full_name(first_name, last_name):
+    """
+    Printing the full name of a given name
+    :param first_name:
+    :param last_name:
+    """
+    print(f"{first_name} {last_name}")
+
+full_name(first_name="John", last_name="Smith")
+
+
+def calculate_age(birthday,name):
+    """
+    Printing the age of a given name
+    :param birthday:
+    :param name:
+    """
+    print(f"name: {name}, age: {2024 - birthday}")
+
+
+calculate_age(birthday=2007, name="Kamoliddin")
+
