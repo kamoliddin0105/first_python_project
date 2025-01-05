75af0e7934bf9278b511399d1afba27942d965cc
kamoliddin0105 <kamoliddinshukurov967@gmail.com>
2025-01-05 08:36:22 +0500

    Functions with returns

--- functions/functions.py
+++ functions/functions.py
@@ -1,8 +1,8 @@
-# print("Salom beruvchi funksiya")
-# def salom_ber(ism):
-#     print(f"Assalomu alaykum {ism.title()}")
-#
-# salom_ber("Kamoliddin")
+print("Salom beruvchi funksiya")
+def salom_ber(ism):
+    print(f"Assalomu alaykum {ism.title()}")
+
+salom_ber("Kamoliddin")
 
 def full_name(first_name, last_name):
     """
@@ -23,6 +23,29 @@ def calculate_age(birthday,name):
     """
     print(f"name: {name}, age: {2024 - birthday}")
 
+
+calculate_age(birthday=2007, name="Kamoliddin")
+
+def create_full_name(first_name, last_name):
+    """
+    Returns a full name given a first name and last name.
+    :param first_name:
+    :param last_name:
+    :return:
+    """
+    full_name = f"{first_name} {last_name}"
+    return full_name
+
+full_name(first_name="John", last_name="Smith")
+
+
+def calculate_age(birthday,name):
+    """
+    Printing the age of a given name
+    :param birthday:
+    :param name:
+    """
+    print(f"name: {name}, age: {2024 - birthday}")
 
 calculate_age(birthday=2007, name="Kamoliddin")
 
